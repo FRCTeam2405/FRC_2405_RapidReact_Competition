9c159ef3fc55b692e747f2d61c6e2b9a2b868568
2405Programmer01 <99556689+2405Programmer01@users.noreply.github.com>
2022-04-05 19:03:28 -0400

    fix on leds

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -308,7 +308,7 @@ public static final class LEDConstants {
     public static final double LED_SETTING_CLIMBER_CLAMP_THREE_BLUE = -0.65;
 
     public static final double LED_SETTING_SHOOT_HIGH = 0.77;
-    // public static final double LED_SETTING_SHOOT_LOW = 0.85;
+    public static final double LED_SETTING_SHOOT_LOW = 0.85;
 
     // Settings
     public static final double LED_SETTING_SHOOTER = -.69;
