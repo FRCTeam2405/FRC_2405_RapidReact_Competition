f8f1e0968a86d75ac65641466c42a7d4418c5906
Moostashgaming <99556514+Moostashgaming@users.noreply.github.com>
2022-04-06 18:35:19 -0400

    Added the shooter hood commands to autonomous

--- src/main/java/frc/robot/commands/AutoPlusOne.java
+++ src/main/java/frc/robot/commands/AutoPlusOne.java
@@ -21,15 +21,25 @@ public class AutoPlusOne extends SequentialCommandGroup {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-      new AutoShootHigh(inSysShooter, inSysFeeder, inSysLEDLights),
+      new ParallelRaceGroup(
+        new LowHood(inSysShooter),
+        new AutoShootHigh(inSysShooter, inSysFeeder, inSysLEDLights)
+      ),
+     
       new CartesianDriveForAuton(inSysDriveTrain, 0, 0, .35, 1.15),
+      
       new ParallelRaceGroup(
         new IntakeAndFeeder(inSysFeeder, inSysIntake),
         new CartesianDriveForAuton(inSysDriveTrain, -.25, 0, 0, 2.5)
         ),
 
       new CartesianDriveForAuton(inSysDriveTrain, 0, 0, .35, 1.15),
-      new AutoShootHigh(inSysShooter, inSysFeeder, inSysLEDLights)
+
+      new ParallelRaceGroup(
+        new HighHood(inSysShooter),
+        new AutoShootHigh(inSysShooter, inSysFeeder, inSysLEDLights)
+      )
+     
     );
   }
 }
